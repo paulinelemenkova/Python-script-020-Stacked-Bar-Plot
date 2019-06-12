c6724c14e754aac5ba518e06731a9b8821fdfd80
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 17:39:51 +0800

    Add files via upload

--- Script-020-Stacked barplots.py
+++ Script-020-Stacked barplots.py
@@ -3,7 +3,6 @@
 
 # In[68]:
 
-
 # libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,9 +15,6 @@ os.chdir('/Users/pauline/Documents/Python')
 df = pd.read_csv("Tab-Morph.csv")
 #df
 
-# y-axis in bold
-#rc('font', weight='bold')
-
 # Values of each group
 bars1 = df.plate_phill
 bars2 = df.plate_pacif
@@ -27,13 +23,12 @@ bars4 = df.plate_carol
  
 # The position of the bars on the x-axis
 profiles = df.profile
- 
-# Names of group and bar width
-names = ["profile1", "profile2", "profile3", "profile4", "profile5", 
-         "profile6", "profile7","profile8", "profile9", "profile10",
-         "profile11", "profile12", "profile13", "profile14", "profile15",
-         "profile16", "profile17", "profile18", "profile19", "profile20",
-         "profile21", "profile22", "profile23", "profile24", "profile25"]
+
+# Generate a loop for 25 profiles
+mylength = range(1,26)
+for i in range(len(mylength)):
+    print("Profile {}".format(i + 1, mylength[i]))
+names = print("Profile {}".format(i + 1, mylength[i]))
 barWidth = 1
  
 # Create bars
@@ -53,19 +48,9 @@ plt.xticks(profiles, names, fontweight='normal', fontsize=7, rotation=30)
 plt.legend()
 ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10), shadow=True, 
           markerscale=2, ncol=4, fontsize=6, title=False)
+ax.set_xlabel('Profiles (1:25)', fontsize=10)
+ax.set_ylabel('Observations (1:518)', fontsize=10)
 plt.title('Mariana Trench: Stacked barplots for the distribution \nof the bathymetric observations across tectonic plates', 
           fontsize=10, fontfamily='sans-serif')
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
